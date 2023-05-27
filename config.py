182d2d259b84ac84ad8891acacf82a9c66cb503b
Darshi Khalasi <127642551+Darshikhalasi@users.noreply.github.com>
2023-05-27 20:17:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,9 +17,6 @@ DISCRIMINATOR_ACTIVATION_INDEX = [
 LEARNING_RATE = 2e-4
 LEARNING_RATE = 2e-4
 EXISTING_DATASETS = {
-    'cityscapes' : 'https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/cityscapes.tar.gz',
-    'edges2handbags' : 'https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/edges2handbags.tar.gz',
-    'edges2shoes' : 'https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/edges2shoes.tar.gz',
-    'facades' : 'https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/facades.tar.gz',
-    'maps' : 'https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/maps.tar.gz'
+    'cityscapes' : 'https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/cityscapes.tar.gz'
+    
 }
